ba8d22519e5e0514cf5234f2a989478832fee2e0
little6523 <67815747+little6523@users.noreply.github.com>
2024-03-19 19:06:30 +0900

    [Bronze III] Title: 대지, Time: 100 ms, Memory: 40768 KB -BaekjoonHub

--- 백준/Bronze/9063. 대지/대지.py
+++ 백준/Bronze/9063. 대지/대지.py
@@ -1,8 +1,20 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 point = [list(map(int, input().split())) for _ in range(n)]
 
-max_x = max(point, key = lambda x:x[0])
-min_x = min(point, key = lambda x:x[0])
-max_y = max(point, key = lambda y:y[1])
-min_y = min(point, key = lambda y:y[1])
-print((max_x[0] - min_x[0]) * (max_y[1] - min_y[1]))
+min_x, min_y = 10000, 10000
+max_x, max_y = -10000, -10000
+for p in point:
+    if p[0] < min_x:
+        min_x = p[0]
+    if p[0] > max_x:
+        max_x = p[0]
+        
+    if p[1] < min_y:
+        min_y = p[1]
+    if p[1] > max_y:
+        max_y = p[1]
+        
+print((max_x-min_x) * (max_y-min_y))
