8d4b573683fa3ff7738ca544b629fd9783b742c7
little6523 <67815747+little6523@users.noreply.github.com>
2024-03-21 14:34:10 +0900

    [Bronze II] Title: 분해합, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,8 +1,15 @@
-n = int(input())
+n = input()
 
-original = 1
+original = int(n) - len(n) * 9
+if original < 0:
+    if int(n) < 10:
+        original = 1
+    else:
+        original = int(n) - (len(n) - 1) * 9
+        
+n = int(n)
 flag = 0
-while original < n:
+while original < int(n):
     splited = list(map(int, str(original)))
     
     if original + sum(splited) == n:
