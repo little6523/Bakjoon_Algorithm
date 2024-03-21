b74064600706d85ee23b2d2a293c22648d5505b7
little6523 <67815747+little6523@users.noreply.github.com>
2024-03-21 15:49:06 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 96 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,35 @@
+n, m = map(int, input().split())
+board = [list(map(str, input())) for _ in range(n)]
+answer = []
+
+W_board = [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
+
+B_board = [['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+           ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+           ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']]
+
+for i in range(n-7):
+    for j in range(m-7):
+        w_cnt = 0; b_cnt = 0
+        for k in range(i, i+8):
+            for l in range(j, j+8):
+                if board[k][l] != W_board[k-i][l-j]:
+                    w_cnt += 1
+                if board[k][l] != B_board[k-i][l-j]:
+                    b_cnt += 1
+        
+        answer.append(min(w_cnt, b_cnt))    
+        
+print(min(answer))
