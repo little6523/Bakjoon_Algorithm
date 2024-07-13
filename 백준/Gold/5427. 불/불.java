400be4072a21301d8cbb4b40f6201127e25686db
LeeHyeonjun <67815747+little6523@users.noreply.github.com>
2024-07-13 18:43:49 +0900

    [Gold IV] Title: 불, Time: 648 ms, Memory: 115648 KB -BaekjoonHub

---
+++ 백준/Gold/5427. 불/불.java
@@ -0,0 +1,155 @@
+import javax.print.attribute.IntegerSyntax;
+import java.util.*;
+import java.io.*;
+import java.util.Queue;
+
+class Main {
+
+    static char[][] map;
+    static Queue<int[]> person = new LinkedList<>();
+    static Queue<int[]> fire = new LinkedList<>();
+    static int[] xDirection = {-1, 0, 1, 0};
+    static int[] yDirection = {0, 1, 0, -1};
+    static int w, h;
+    static int time = 0;
+    static boolean isPossible = false;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < t; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            map = new char[h][w];
+
+            String temp;
+            for (int i = 0; i < h; i++) {
+                temp = br.readLine();
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = temp.charAt(j);
+                    if (temp.charAt(j) == '@') {
+                        person.offer(new int[]{i, j});
+                    }
+
+                    if (temp.charAt(j) == '*') {
+                        fire.offer(new int[]{i, j});
+                    }
+                }
+            }
+
+            while (true) {
+//                System.out.println("현재시간: " + (time + 1));
+                bfs();
+//                print();
+                if (isPossible) {
+                    System.out.println(time + 1);
+                    time = 0;
+                    isPossible = false;
+                    person.clear();
+                    fire.clear();
+                    break;
+                }
+
+                else {
+                    if (person.isEmpty()) {
+                        System.out.println("IMPOSSIBLE");
+                        time = 0;
+                        fire.clear();
+                        break;
+                    }
+                }
+                time++;
+            }
+        }
+    }
+
+    static void bfs() {
+        int size = fire.size();
+
+        for (int i = 0; i < size; i++) {
+            if (!fire.isEmpty()) {
+                int[] newFire = fire.poll();
+
+                for (int j = 0; j < 4; j++) {
+                    int nx = newFire[0] + xDirection[j];
+                    int ny = newFire[1] + yDirection[j];
+
+                    if (nx < 0 || nx >= h || ny < 0 || ny >= w) {
+                        continue;
+                    }
+
+                    if (map[nx][ny] == '*' || map[nx][ny] == '#') {
+                        continue;
+                    }
+
+                    map[nx][ny] = '*';
+                    fire.offer(new int[]{nx, ny});
+                }
+            }
+        }
+
+        size = person.size();
+        for (int i = 0; i < size; i++) {
+            if (!person.isEmpty()) {
+                int[] newPerson = person.poll();
+                if (map[newPerson[0]][newPerson[1]] == '.') {
+                    map[newPerson[0]][newPerson[1]] = '@';
+                }
+
+                if (newPerson[0] == 0 || newPerson[0] == h - 1 || newPerson[1] == 0 || newPerson[1] == w - 1) {
+                    isPossible = true;
+                    return;
+                }
+
+                for (int j = 0; j < 4; j++) {
+                    int nx = newPerson[0] + xDirection[j];
+                    int ny = newPerson[1] + yDirection[j];
+
+                    if (nx < 0 || nx >= h || ny < 0 || ny >= w) {
+                        continue;
+                    }
+
+                    if (map[nx][ny] == '*' || map[nx][ny] == '#' || map[nx][ny] == '@') {
+                        continue;
+                    }
+
+                    if (nx == 0 || nx == h - 1 || ny == 0 || ny == w - 1) {
+                        isPossible = true;
+                        time++;
+                        return;
+                    }
+
+                    map[nx][ny] = '@';
+                    person.offer(new int[]{nx, ny});
+//                    printPerson();
+                }
+            }
+        }
+    }
+
+    static void print() {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void printPerson() {
+        for (int[] arr : person) {
+            System.out.print(Arrays.toString(arr) + " ");
+        }
+        System.out.println();
+    }
+
+    static void printFire() {
+        for (int[] arr : fire) {
+            System.out.print(Arrays.toString(arr) + " ");
+        }
+        System.out.println();
+    }
+}
