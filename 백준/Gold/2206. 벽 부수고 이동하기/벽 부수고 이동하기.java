3378503f2187442f9790802c33939b349149c8a6
LeeHyeonjun <67815747+little6523@users.noreply.github.com>
2024-07-16 02:53:36 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 560 ms, Memory: 123780 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,79 +1,76 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-class Node{
-	int x;
-	int y;
-	int dist;
-	int boom;
-	Node(int x, int y, int dist, int boom){
-		this.x = x;
-		this.y = y;
-		this.dist = dist;
-		this.boom = boom;
-	}
-}
+public class Main {
 
-public class Main{
+    static int n, m;
+    static int[][] map;
+    static boolean[][][] visited;
+    static Queue<int[]> queue = new LinkedList<>();
+    static int[] xDirection = {-1, 0, 1, 0};
+    static int[] yDirection = {0, 1, 0, -1};
+    static int total = -1;
 
-	static final int dx[] = {0,0,1,-1};
-	static final int dy[] = {1,-1,0,0};
-	static int n,m,ans;
-	static boolean visit[][][];
-	static int map[][];
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
-		visit = new boolean[n][m][2];
-		ans = -1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        visited = new boolean[2][n][m];
 
-		for(int i=0; i<n; i++) {
-			String s = br.readLine();
-			for(int j=0; j<m; j++) {
-				map[i][j] = s.charAt(j) - '0';
-			}
-		}
-		bfs();
-		
-		System.out.println(ans);
-			
-	}
-	
-	static void bfs() {
-		Queue<Node> q = new LinkedList<>();
-		q.add(new Node(0,0,1,0));
-		visit[0][0][0] = true;
-		
-		while(!q.isEmpty()) {
-			Node now = q.poll();
-			int x = now.x;
-			int y = now.y;
-			
-			if(x==n-1 && y == m-1) {
-				ans = now.dist;
-				return;
-			}
-			
-			for(int k=0; k<4; k++) {
-				int nx = x + dx[k];
-				int ny = y + dy[k];
-				if(0>nx || nx>=n || 0>ny || ny>=m) continue;
-				
-				if(map[nx][ny] == 0 && !visit[nx][ny][now.boom]) { //벽이 아닐경우
-					visit[nx][ny][now.boom] = true;
-					q.add(new Node(nx,ny,now.dist+1,now.boom));
-				}else { //벽이 있는경우
-					if(now.boom == 0 && !visit[nx][ny][now.boom+1]) { //벽을 부순적이 없을때
-						visit[nx][ny][now.boom+1] = true;
-						q.add(new Node(nx,ny,now.dist+1,now.boom+1));
-					}
-				}
-				
-			}
-		}
-		
-	}
+        for (int i = 0; i < n; i++) {
+            String temp = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = temp.charAt(j) - '0';
+            }
+        }
+
+        queue.offer(new int[]{0, 0, 1, 0});
+        visited[0][0][0] = true;
+
+        bfs();
+
+        System.out.println(total);
+    }
+
+    static void bfs() {
+
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
+            int x = now[0];
+            int y = now[1];
+            int dist = now[2];
+            int destroy = now[3];
+
+            if (x == n - 1 && y == m - 1) {
+                total = dist;
+                return;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + xDirection[i];
+                int ny = y + yDirection[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
+                    continue;
+                }
+
+                if (map[nx][ny] == 0 && !visited[destroy][nx][ny]) {
+                    visited[destroy][nx][ny] = true;
+                    queue.offer(new int[]{nx, ny, dist + 1, destroy});
+                } else {
+                    if (destroy == 0 && !visited[destroy + 1][nx][ny]) {
+                        visited[destroy + 1][nx][ny] = true;
+                        queue.offer(new int[]{nx, ny, dist + 1, destroy + 1});
+                    }
+                }
+            }
+        }
+    }
 }
