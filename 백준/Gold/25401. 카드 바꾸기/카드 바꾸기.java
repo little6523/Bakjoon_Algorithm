0d1ea304adf05d6d529cbb0e136d626fd816ff11
LeeHyeonjun <67815747+little6523@users.noreply.github.com>
2024-08-15 03:29:55 +0900

    [Gold V] Title: 카드 바꾸기, Time: 92 ms, Memory: 14224 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Gold/25401. 카드 바꾸기/카드 바꾸기.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> diffMap = new HashMap<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i < n + 1; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num;
+
+            if (!map.containsKey(num)) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+        }
+
+        int answer;
+
+        // 1. 가장 많이 나온 숫자를 제외한 나머지 숫자를 가장 많이 나온 숫자로 바꾸는 경우
+        int most = 0;
+        List<Integer> list = new ArrayList<>(map.keySet());
+        for (int num : list) {
+            if (map.get(num) > most) {
+                most = map.get(num);
+            }
+        }
+
+        answer = n - most;
+//        System.out.println(answer);
+
+        // 2. 등차수열 만드는 경우
+
+        int min = n - 1;
+        for (int i = 1; i < n; i++) {
+            int diff = arr[i + 1] - arr[i];
+            int a0 = arr[i] - (i - 1) * diff;
+//            System.out.println("초항: " + a0);
+            int[] copyArr = arr.clone();
+
+            for (int j = 1; j < n + 1; j++) {
+                copyArr[j] = a0 + (j - 1) * diff;
+            }
+
+//            System.out.println(Arrays.toString(arr) + " " + Arrays.toString(copyArr));
+
+            int check = differenceCheck(arr, copyArr, n);
+            if (check < min) {
+                min = check;
+            }
+        }
+
+        if (answer > min) {
+            answer = min;
+        }
+
+        System.out.println(answer);
+    }
+
+    public static int differenceCheck(int[] arr1, int[] arr2, int n) {
+
+        int count = 0;
+        for (int i = 1; i < n + 1; i++) {
+            if (arr1[i] != arr2[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
