8d255d5f33bfc1b7cfd647076da1fd51ff3daf44
LeeHyeonjun <67815747+little6523@users.noreply.github.com>
2024-08-21 02:36:29 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 128 ms, Memory: 17024 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -5,10 +5,9 @@ import java.util.*;
 
 public class Main {
 
-    public static Queue<Pair> queue = new LinkedList<>();
+    public static Queue<int[]> queue = new LinkedList<>();
     public static int n, m;
     public static int[][] conn, steps;
-    public static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,13 +17,8 @@ public class Main {
 
         conn = new int[n + 1][n + 1];
         steps = new int[n + 1][n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                steps[i][j] = m;
-                steps[i][i] = 0;
-            }
-        }
 
+        // 관계 정보를 입력받으면서 connection(관계), steps(단계) 초기화
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -32,38 +26,33 @@ public class Main {
             conn[a][b] = 1; conn[b][a] = 1;
             steps[a][b] = 1; steps[b][a] = 1;
         }
-        
-        bfs();
 
-        Collections.sort(list);
-        System.out.println(list.get(0));
+        System.out.println(bfs());
     }
 
-    public static void bfs() {
+    public static int bfs() {
 
-        int min = Integer.MAX_VALUE;
+        int min = Integer.MAX_VALUE, answer = n;
         for (int i = 1; i < n + 1; i++) {
             initQueue(queue, i);
             boolean[] visited = new boolean[n + 1];
             visited[i] = true;
 
             while (!queue.isEmpty()) {
-                Pair pair = queue.poll();
-                int friend = pair.friend;
-                int step = pair.step;
-
-//                System.out.println(i+ " " + friend + " " + step);
+                int[] pair = queue.poll();
+                int friend = pair[0];
+                int step = pair[1];
 
                 if (visited[friend]) continue;
                 visited[friend] = true;
 
-                if (step < steps[i][friend]) {
+                if (step < steps[i][friend] || steps[i][friend] == 0) {
                     steps[i][friend] = step;
                 }
 
                 for (int j = 1; j < n + 1; j++) {
                     if (conn[friend][j] == 1 && !visited[j]) {
-                        queue.offer(new Pair(j, step + 1));
+                        queue.offer(new int[]{j, step + 1});
                     }
                 }
             }
@@ -77,31 +66,17 @@ public class Main {
 
             if (sum < min) {
                 min = sum;
-                list.clear();
-                list.add(i);
-            }
-
-            else if (sum == min) {
-                list.add(i);
+                answer = i;
             }
         }
+        return answer;
     }
 
-    public static void initQueue(Queue<Pair> queue, int index) {
+    public static void initQueue(Queue<int[]> queue, int index) {
         for (int i = 1; i < n + 1; i++) {
             if (conn[index][i] == 1) {
-                queue.offer(new Pair(i, 1));
+                queue.offer(new int[]{i, 1});
             }
-        }
-    }
-
-    public static class Pair {
-        public int friend;
-        public int step;
-
-        public Pair(int friend, int step) {
-            this.friend = friend;
-            this.step = step;
         }
     }
 }
